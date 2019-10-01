08305838f8438b8c36a7517f4712aa1cc1d03ae6
Antony Alex <antonyalexcm@gmail.com>
2019-10-01 10:42:31 +0530

    First commit of the addressbook

---
+++ Landmark.py
@@ -0,0 +1,8 @@
+from Address import(
+    Address
+)
+
+class LandmarkAddress:
+    def __init__(self,landmark_name):
+        self.landmark_name = landmark_name
+        self.address = None
---
+++ person.py
@@ -0,0 +1,9 @@
+from Address import(
+    Address
+)
+
+class PersonAddress:
+    def __init__(self,first_name, second_name):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.address = None
